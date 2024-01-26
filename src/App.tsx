eb97ac22af3d85707ce9aca9d4b02caf34cb8aa8
Shakhzod Kudratov <shakhzodkudratov@gmail.com>
2024-01-26 17:23:12 +0500

    mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import { AppShell, Burger } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 import React from 'react';
 
 const App: React.FC = () => {
-  return <div className="bg-red-50 text-2xl">hello</div>;
+  const [opened, { toggle }] = useDisclosure();
+
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: 'sm',
+        collapsed: { mobile: !opened },
+      }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+        <div>Logo</div>
+      </AppShell.Header>
+
+      <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+
+      <AppShell.Main>
+        <div className="bg-red-50 text-2xl">hello</div>
+      </AppShell.Main>
+    </AppShell>
+  );
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { createTheme, MantineProvider } from '@mantine/core';
 import './index.css';
+import '@mantine/core/styles.css';
+
+const theme = createTheme({});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider theme={theme}>
+      <App />
+    </MantineProvider>
   </React.StrictMode>,
 );
