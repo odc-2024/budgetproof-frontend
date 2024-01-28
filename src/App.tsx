4ee45387ae7c1632dd8236307dbbb492d31a8665
mhw0 <mhw0@yahoo.com>
2024-01-28 09:33:37 +0500

    feat: metamask and myid auth modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { router } from '@/router';
+import { Modal, Button, Image, Flex, Text } from '@mantine/core';
 
 const App: React.FC = () => {
-  return <RouterProvider router={router} />;
+  return <div>
+    <Modal opened={false} onClose={() => {}} title="Authorization" centered>
+      <Flex justify={'center'} gap={20}>
+        <Button h={200} color='#EEEDEB'>
+          <Flex direction={'column'} align={'center'}>
+            <Image src={'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/MetaMask_Fox.svg/1024px-MetaMask_Fox.svg.png'} w={150} />
+            <Text color='black'>MetaMask</Text>
+          </Flex>
+        </Button>
+
+        <Button h={200} color='#EEEDEB' onClick={() => { window.location.href = 'http://localhost:3000'; }}>
+          <Flex direction={'column'} align={'center'}>
+            <Image src={'https://play-lh.googleusercontent.com/Pcn6hSM1uFWHDwE_JE8PYVGglnUGqMmvYzhcpJedEZDu7toPScEgWnI2A_2mOqVQEEM'} w={150} />
+            <Text color='black'>MyID.uz</Text>
+          </Flex>
+        </Button>
+      </Flex>
+    </Modal>
+    <RouterProvider router={router} />;
+  </div>
 };
 
 export default App;
