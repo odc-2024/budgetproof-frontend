111140acb752f4ddad723c5a921ae2276426b87f
mhw0 <mhw0@yahoo.com>
2024-01-28 11:39:39 +0500

    wip

--- src/views/CampaignsCardView.tsx
+++ src/views/CampaignsCardView.tsx
@@ -1,25 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DefaultLayout from '@/components/Layout/DefaultLayout.tsx';
-import { Group, Stack, /*Text,*/ Title, Button } from '@mantine/core';
+import { Group, Stack, Title, Button, Table } from '@mantine/core';
+import { Campaign, contract } from '@/contract';
 /*
 import CampaignsCard from '@/components/campaigns/CampaignsCard.tsx';
 import UsersView from '@/views/UsersView.tsx';
 */
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 const CampaignsCardView: React.FC = () => {
+  const [campaign, setCampaign] = useState<Campaign | null>(null);
+  const [allocations, setAllocations] = useState<any | null>(null);
+  const params = useParams()
+  const allocationRows = [];
+
+  React.useEffect(() => {
+    contract.getCampaign(params.id).then((campaign) => {
+      setCampaign(campaign);
+    });
+
+    contract.getAllocations(params.id).then((allocations) => {
+      setAllocations(allocations);
+    });
+  }, []);
+
   return (
     <DefaultLayout>
       <div className="container mx-auto px-5">
         <Stack>
           <Group justify="space-between">
-            <Title order={1}>Campaign View</Title>
+            <div>
+              <Title order={1}>{campaign?.name}</Title>
+              <Title size={20} fw={0} order={1}>{campaign?.remainingAmount.toLocaleString()} / {campaign?.amount.toLocaleString()} {campaign?.unit}</Title>
+            </div>
             <Link to="/campaigns">
               <Button px={40} variant="outline">
                 Back
               </Button>
             </Link>
           </Group>
+
+          <div>
+
+            <Table>
+              <Table.Thead>
+                <Table.Tr>
+                  <Table.Th>ID</Table.Th>
+                  <Table.Th>Receiver</Table.Th>
+                  <Table.Th>Amount</Table.Th>
+                  <Table.Th>Sender</Table.Th>
+                </Table.Tr>
+              </Table.Thead>
+              <Table.Tbody>
+                { allocationRows }
+              </Table.Tbody>
+            </Table>
+          </div>
+
           { /* 
           <CampaignsCard>
             <Stack gap={20}>
