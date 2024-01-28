5475d84dd31723ffd029b50e989324620b37e263
mhw0 <mhw0@yahoo.com>
2024-01-28 11:39:39 +0500

    wip

--- src/contract.ts
+++ src/contract.ts
@@ -2,7 +2,9 @@ import { ContractTransactionReceipt, ethers } from "ethers";
 
 const CONTRACT_ABI = [
   "function getBudgets() public view returns((uint256, string, uint256, string, uint256, address)[])",
-  "function createBudget(string memory name_, uint256 amount_, string memory unit_) public returns (uint256)"
+  "function createBudget(string memory name_, uint256 amount_, string memory unit_) public returns (uint256)",
+  "function getBudget(uint256 budgetId_) public view returns((uint256, string, uint256, string, uint256, address))",
+  "function getAllocations(uint256 budgetId_) public view returns((uint256, string, address, uint256, string, address)[])",
 ];
 
 export interface Campaign {
@@ -20,7 +22,7 @@ class Contract {
 
   constructor() {
     this.browserProvider = new ethers.BrowserProvider((window as any).ethereum);
-    this.contract = new ethers.Contract("0xc3e53F4d16Ae77Db1c982e75a937B9f60FE63690", CONTRACT_ABI, this.browserProvider);
+    this.contract = new ethers.Contract("0xA4899D35897033b927acFCf422bc745916139776", CONTRACT_ABI, this.browserProvider);
   }
 
   public deserializeCampaign(budget: [bigint, string, bigint, string, bigint, string]): Campaign {
@@ -34,8 +36,26 @@ class Contract {
     }
   }
 
+  public deserializeAllocation(allocation: [bigint, string, bigint, string, string, string]): any {
+    console.log(allocation);
+    /*
+    return {
+      state: allocation[0],
+      name: budget[1],
+      amount: budget[2],
+      unit: budget[3],
+      remainingAmount: budget[4],
+      contractAddress: budget[5]
+    }
+    */
+  }
+
   public async getCampaigns(): Promise<Campaign[]> {
     return (await this.contract["getBudgets"]()).map(this.deserializeCampaign);
+  }
+
+  public async getAllocations(budgetId: number): Promise<Campaign[]> {
+    return (await this.contract["getAllocations"](budgetId)).map(this.deserializeAllocation);
   }
 
   public async getCampaign(campaignId: number): Promise<Campaign> {
